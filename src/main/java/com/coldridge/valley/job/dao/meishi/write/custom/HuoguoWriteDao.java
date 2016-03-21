a78c2ef1e7b6b47bb40bc801d6480d03d66be706
Jazzylol <506496088@qq.com>
2016-03-22 02:48:57 +0800

    成功编译！

---
+++ src/main/java/com/coldridge/valley/job/dao/meishi/write/custom/HuoguoWriteDao.java
@@ -0,0 +1,6 @@
+package com.coldridge.valley.job.dao.meishi.write.custom;
+
+import com.coldridge.valley.job.dao.meishi.write.mbg.MBGHuoguoWriteDao;
+
+public interface HuoguoWriteDao extends MBGHuoguoWriteDao {
+}
---
+++ src/main/java/com/coldridge/valley/job/model/po/mbg/BaseExample.java
@@ -0,0 +1,23 @@
+package com.coldridge.valley.job.model.po.mbg;
+
+public class BaseExample {
+    private Long start;
+
+    private Long limit;
+
+    public void setStart(Long start) {
+        this.start=start;
+    }
+
+    public Long getStart() {
+        return start;
+    }
+
+    public void setLimit(Long limit) {
+        this.limit=limit;
+    }
+
+    public Long getLimit() {
+        return limit;
+    }
+}
